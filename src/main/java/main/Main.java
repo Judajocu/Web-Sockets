bd6e4a920ae273c466a5f13c99216336179f50af
Melisa <melissalantigua@hotmail.com>
2018-06-10 14:45:17 -0400

    agregado template

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,8 +5,31 @@ import freemarker.template.Template;
 import spark.*;
 import spark.template.freemarker.FreeMarkerEngine;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.staticFiles;
+
 public class Main {
     public static void main(String[] args){
-
+        new Main().manejadorFremarker();
     }
+
+    public void manejadorFremarker(){
+
+        staticFiles.location("/templates");
+
+        Configuration configuration= new Configuration(Configuration.VERSION_2_3_23);
+        configuration.setClassForTemplateLoading(
+                Main.class,"/templates/");
+        FreeMarkerEngine motor= new FreeMarkerEngine(configuration);
+
+        get("/", (request, response) -> {
+            Map<String, Object> mapa = new HashMap<>();
+            mapa.put("name","Melissa Lantigua - 2014-0805");
+            return new ModelAndView(mapa, "inicio.ftl");
+        }, motor);
+    }
+
 }
